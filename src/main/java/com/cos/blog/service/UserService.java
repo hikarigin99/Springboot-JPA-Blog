be6680a63de0ede04ec198f766dc31e1fb59a5ba
hikarigin99 <hikarigin99@gmail.com>
2021-03-31 16:33:34 +0900

    6강완료

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.cos.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
@@ -14,13 +16,15 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 	@Transactional
 	public void 회원가입(User user) {
+		String rawPassword = user.getPassword();	//1234원문
+		String encPassword = encoder.encode(rawPassword);	// 해쉬화
+		user.setPassword(encPassword);
+		user.setRole(RoleType.USER);
 		userRepository.save(user);
-	}
-	
-	@Transactional(readOnly = true)	// Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료(정합성)
-	public User 로그인(User user) {
-		return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
 	}
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,9 +3,6 @@ let index = {
 			$("#btn-save").on("click", ()=>{  //function(){}, ()=>{} this를 바인딩하기 위해서!!
 				this.save();
 			});
-			$("#btn-login").on("click", ()=>{  //function(){}, ()=>{} this를 바인딩하기 위해서!!
-				this.login();
-			});
 		},
 	
 		save: function() {
@@ -24,39 +21,18 @@ let index = {
 			$.ajax({
 				//회원가입 수행요청(100초 가정)
 				type: "POST",
-				url: "/blog/api/user",
+				url: "/auth/joinProc",
 				data: JSON.stringify(data),  // http body데이터
 				contentType: "application/json; charset=utf-8",  // body데이터가 어떤 타입인지(MIME)
 				dataType: "json"	// 요청을 서버로해서 응답이 왔을 때 기본적으로 모든것이 String(생긴게 json이라면) => javascript오브젝트로 변경
 			}).done(function(resp){
 				alert("회원가입이 완료되었습니다.");
 				//console.log(resp);
-				location.href="/blog";
+				location.href="/";
 			}).fail(function(error) {
 				alert(JSON.stringify(error));
 			});	
 		},
-		
-		login: function() {
-			//alert('user의 save함수 호출됨');
-			let data = {
-					username: $("#username").val(),
-					password: $("#password").val(),
-			};
-	
-			$.ajax({
-				type: "POST",
-				url: "/blog/api/user/login",
-				data: JSON.stringify(data),  // http body데이터
-				contentType: "application/json; charset=utf-8",  // body데이터가 어떤 타입인지(MIME)
-				dataType: "json"	// 요청을 서버로해서 응답이 왔을 때 기본적으로 모든것이 String(생긴게 json이라면) => javascript오브젝트로 변경
-			}).done(function(resp){
-				alert("로그인이 완료되었습니다.");
-				location.href="/blog";
-			}).fail(function(error) {
-				alert(JSON.stringify(error));
-			});	
-		}
 }
 
 index.init();
